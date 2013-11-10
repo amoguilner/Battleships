7623856b870d9f65cc83349f4d4676a8f5ab7a57
amoguilner <alex_x86@yahoo.com>
2013-11-10 16:28:54 -0600

    more changes

--- Battleships/src/gamelogictests/ShipTest.java
+++ Battleships/src/gamelogictests/ShipTest.java
@@ -19,7 +19,7 @@ public class ShipTest
 	@Test
 	public void testBattleshipCreation()
 	{
-		Ship ship = new Battleship(new Board(10), 1, 1, ShipVector.Right);
+		Battleship ship = new Battleship(new Board(10), 1, 1, ShipVector.Right);
 		
 		assertTrue("Battleship size is not 4.", ship.getSize() == 4);
 		assertTrue("Battleship doesn't have a class type key.", ship.getShipTypeKey() != "");
@@ -30,7 +30,7 @@ public class ShipTest
 	@Test
 	public void testCruiserCreation()
 	{
-		Ship ship = new Cruiser(new Board(10), 1, 1, ShipVector.Right);
+		Cruiser ship = new Cruiser(new Board(10), 1, 1, ShipVector.Right);
 		
 		assertTrue("Cruiser size is not 3.", ship.getSize() == 3);
 		assertTrue("Cruiser must have a class type.", ship.getShipTypeKey() != "");
@@ -41,7 +41,7 @@ public class ShipTest
 	@Test
 	public void testDestroyerCreation()
 	{
-		Ship ship = new Destroyer(new Board(10), 1, 1, ShipVector.Right);
+		Destroyer ship = new Destroyer(new Board(10), 1, 1, ShipVector.Right);
 		
 		assertTrue("Destroyer size is not 2.", ship.getSize() == 2);
 		assertTrue("Destroyer must have a class type.", ship.getShipTypeKey() != "");
@@ -52,7 +52,7 @@ public class ShipTest
 	@Test
 	public void testSubmarineCreation()
 	{
-		Ship ship = new Submarine(new Board(10), 1, 1, ShipVector.Right);
+		Submarine ship = new Submarine(new Board(10), 1, 1, ShipVector.Right);
 		
 		assertTrue("Submarine size is not 1.", ship.getSize() == 1);
 		assertTrue("Submarine must have a class type.", ship.getShipTypeKey() != "");
@@ -63,7 +63,7 @@ public class ShipTest
 	@Test
 	public void testMineCreation()
 	{
-		Ship ship = new Mine(new Board(10), 1, 1, ShipVector.Right);
+		Mine ship = new Mine(new Board(10), 1, 1, ShipVector.Right);
 		
 		assertTrue("Mine size is not 1.", ship.getSize() == 1);
 		assertTrue("Mine must have a class type.", ship.getShipTypeKey() != "");
@@ -74,7 +74,7 @@ public class ShipTest
 	@Test
 	public void testMinesweeperCreation()
 	{
-		Ship ship = new Minesweeper(new Board(10), 1, 1, ShipVector.Right);
+		Minesweeper ship = new Minesweeper(new Board(10), 1, 1, ShipVector.Right);
 		
 		assertTrue("Minesweeper size is 0.", ship.getSize() == 1);
 		assertTrue("Minesweeper must have a class type.", ship.getShipTypeKey() != "");
